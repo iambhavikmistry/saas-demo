8ba4961ce7af2afd8e111f38e5391504c4dc86cf
Bhavik Mistry <43392058+iambhavikmistry@users.noreply.github.com>
2024-10-27 02:53:27 +0530

    Initial commit

---
+++ wave/src/Http/Controllers/Billing/Webhooks/PaddleWebhook.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wave\Http\Controllers\Billing\Webhooks;
+
+use Wave\Plan;
+use Wave\Subscription;
+use Illuminate\Http\Request;
+use Stripe\Checkout\Session;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+class PaddleWebhook extends Controller
+{
+    public $paddle_url;
+    public function handler(Request $request)
+    {
+        $event = $request->get('event_type', null);
+
+        dump('hit the handler');
+        dump($event);
+
+        dump('request');
+        dump($request);
+
+        Log::info('hit the handler');
+
+        switch ($event) {
+            case 'subscription.canceled':
+                $this->subscriptionCancelled($request);
+                break;
+            default:
+                break;
+        }
+    
+        return response()->json(['message' => 'Webhook handled successfully'], 200);
+
+    }
+
+    protected function subscriptionCancelled(Request $request)
+    {
+        $subscriptionId = $request->input('data.id'); // Adjusted to match the payload structure
+
+        // Ensure the subscription ID is provided
+        if (is_null($subscriptionId)) {
+            Log::warning('Subscription ID missing in subscriptionCancelled webhook.');
+            return;
+        }
+
+        $subscription = Subscription::where('vendor_subscription_id', $subscriptionId)->where('status', 'active')->first();
+        $subscription->cancel();
+    }
+}
